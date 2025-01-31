22bcbd9ea432f25b6f13c252c8d78d1b8c8e2c05
raju61 <raju.marmam61@gmail.com>
2025-01-31 09:20:02 +0530

    declaration

--- list_practice.py
+++ list_practice.py
@@ -1,1 +1,27 @@
+'''
+list is a ordered collection of any type of data type. whether it can be string, integer, boolean, float, list,tuple,dictionary etc.
+separated with comma. defined with [ ].
+ordered, mutable,indexed, and allow duplicates.
+'''
+lst=[1,2,3,96.53,1524,'Raju',"Marmam",'''HYD''',[3,[2]],{34},(456)]
+# print(lst)
+# print(type(lst))
+print(len(lst))
+# lst=list((1,2,3,96.53,1524,'Raju',"Marmam",'''HYD''',[3,[2]],{34},(456)))
+# print(type(lst))
+
+'''
+different data types as input from the user
+literal_eval() - used to check whether the input element is string, float, int, list, tuple, dict
+
+'''
+# import ast
+# lst=input("Enter elements : ").split(',')
+# def my_list(lst):
+#     result=[ast.literal_eval(item.strip()) for item in lst]
+#     return result
+# op=my_list(lst)
+# print(op)
+# print(type(op))
+
 
